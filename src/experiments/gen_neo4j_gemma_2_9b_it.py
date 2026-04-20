6c111099a51d1e14ee609a355914196937dd05cb
ashkihotah <nicoloresta02@gmail.com>
2026-04-20 17:56:29 +0200

    added script for generating cyphers from neo4j-gemma-2b-it baseline

---
+++ src/experiments/gen_neo4j_gemma_2_9b_it.py
@@ -0,0 +1,179 @@
+import argparse
+import os
+from typing import override
+
+import pandas as pd
+
+
+
+
+import torch
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+)
+
+import os
+from dotenv import load_dotenv
+
+from experiments.utils import add_agent_args, read_df, read_yaml_config
+from experiments.patterns import (
+    DfToDfGenerator,
+)
+
+load_dotenv()
+
+class CypherGenerator(DfToDfGenerator):
+    
+    @override
+    def __init__(
+        self,
+        in_df: pd.DataFrame,
+        join_column: str,
+        out_df: pd.DataFrame = None,
+    ):
+        super().__init__(
+            in_df=in_df,
+            join_column=join_column,
+            out_df=out_df,
+        )
+
+        model_name = "neo4j/text2cypher-gemma-2-9b-it-finetuned-2024v1"
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.bfloat16,
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            quantization_config=bnb_config,
+            torch_dtype=torch.bfloat16,
+            attn_implementation="eager",
+            low_cpu_mem_usage=True,
+            token=os.getenv("HF_TOKEN"),
+        )
+        self.user_instruction = (
+            "Generate Cypher statement to query a graph database. "
+            "Use only the provided relationship types and properties in the schema. \n"
+            "Schema: {schema} \n Question: {question}  \n Cypher output: "
+        )
+        self.model_generate_parameters = {
+            "top_p": 0.9,
+            "temperature": 0.2,
+            "max_new_tokens": 512,
+            "do_sample": True,
+            "pad_token_id": self.tokenizer.eos_token_id,
+        }
+
+    def prepare_chat_prompt(self, question, schema) -> list[dict]:
+        chat = [
+            # {
+            #     "role": "system",
+            #     "content": self.system_instructions,
+            # },
+            {
+                "role": "user",
+                "content": self.user_instruction.format(
+                    schema=schema, question=question
+                ),
+            }
+        ]
+        return chat
+
+    def _postprocess_output_cypher(self, output_cypher: str) -> str:
+        # Remove any explanation. E.g.  MATCH...\n\n**Explanation:**\n\n -> MATCH...
+        # Remove cypher indicator. E.g.```cypher\nMATCH...```` --> MATCH...
+        # Note: Possible to have both:
+        #   E.g. ```cypher\nMATCH...````\n\n**Explanation:**\n\n --> MATCH...
+        partition_by = "**Explanation:**"
+        output_cypher, _, _ = output_cypher.partition(partition_by)
+        output_cypher = output_cypher.strip("`\n")
+        output_cypher = output_cypher.lstrip("cypher\n")
+        output_cypher = output_cypher.strip("`\n ")
+        return output_cypher
+
+    @override
+    def generate_output_record(self, input_record: dict) -> dict:
+        record_id = str(input_record[self.join_column])
+
+        question = input_record['question']
+        schema = input_record['schema']
+        new_message = self.prepare_chat_prompt(question=question, schema=schema)
+        prompt = self.tokenizer.apply_chat_template(new_message, add_generation_prompt=True, tokenize=False)
+        inputs = self.tokenizer(prompt, return_tensors="pt", padding=True)
+
+        inputs.to(self.model.device)
+        self.model.eval()
+        with torch.no_grad():
+            tokens = self.model.generate(**inputs, **self.model_generate_parameters)
+            tokens = tokens[:, inputs.input_ids.shape[1] :]
+            raw_outputs = self.tokenizer.batch_decode(tokens, skip_special_tokens=True)
+            outputs = [self._postprocess_output_cypher(output) for output in raw_outputs]
+
+        return_dict = {
+            self.join_column: record_id,
+            "json_trajectory": None,
+            "generated_cypher": outputs[0],
+        }
+
+        return return_dict
+
+def init_parser():
+    parser = argparse.ArgumentParser(description="Generate synthetic data analysis.")
+    add_agent_args(parser)
+
+    parser.add_argument("--input-df", type=str, required=True,
+        help="Path to the input .csv or .parquet data file.",
+    )
+    parser.add_argument("--input-df-yaml-config", type=str, default=None,
+        help="Path to the YAML config file for reading the input dataframe.",
+    )
+    parser.add_argument("--output-df", type=str, required=True,
+        help="Path to the output file where generated data will be saved.",
+    )
+    parser.add_argument("--output-df-yaml-config", type=str, default=None,
+        help="Path to the YAML config file for saving the output dataframe.",
+    )
+    parser.add_argument("--join-column", type=str, default="df.index",
+        help="Column name to join input and output dataframes during resumption.",
+    )
+    parser.add_argument("--user-instruction", type=str, default="You are a helpful assistant.",
+        help="The user instruction to use for data generation. " \
+        "It must contain the placeholders {schema} and {question} for schema and question respectively.",
+    )
+    parser.add_argument("--generation-params", type=str, default=None,
+        help="Path to the YAML config file for model generation parameters.",
+    )
+    parser.add_argument("--verbose", type=bool, default=True,
+        help="Enable verbose logging.",
+    )
+
+    return parser
+
+def main():
+    parser = init_parser()
+    args = parser.parse_args()
+
+    if os.path.exists(args.output_df):
+        output_df = read_df(args.output_df)
+        # output_df = output_df.dropna()
+    else:
+        output_df = None
+
+    input_df = read_df(args.input_df, **read_yaml_config(args.input_df_yaml_config))
+    input_df = input_df[input_df['database_reference_alias'].notna()]#[:100]
+    generator = CypherGenerator(
+        in_df=input_df,
+        join_column=args.join_column,
+        out_df=output_df,
+    )
+    generator.generate(
+        save_path=args.output_df,
+        **read_yaml_config(args.output_df_yaml_config),
+    )
+
+if __name__ == "__main__":
+    main()
